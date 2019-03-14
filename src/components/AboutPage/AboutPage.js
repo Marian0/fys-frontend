f9bdf72dad091dc7a8df37ba1e926e5a1c0617c7
marian0 <marianosantafe@gmail.com>
2019-03-13 22:48:18 -0300

    About page enhancements

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
-const AboutPage = () => (<h1>About Page</h1>);
+const AboutPage = () => (
+    <Fragment>
+        <h1>About Page</h1>
+
+        <p>React + Redux Challenge</p>
+
+        <p><strong>Author</strong> <a href="https://www.linkedin.com/in/marianopeyregne/">Mariano Peyregne</a></p>
+    </Fragment>
+);
 
 export default AboutPage;
